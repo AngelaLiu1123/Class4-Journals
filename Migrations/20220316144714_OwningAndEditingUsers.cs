661edcfee39a964c7d51b8fa2a7bff2a4fd7eb30
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-03-16 09:54:24 -0500

    Add Owning and Editing Users to Journal, Add Created time to Journal, add migrations.

---
+++ Migrations/20220316144714_OwningAndEditingUsers.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Class4_Journals.Migrations
+{
+    public partial class OwningAndEditingUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Journal_User_UserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Journal_UserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropColumn(
+                name: "UserNumber",
+                table: "Journal");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EditingUserNumber",
+                table: "Journal",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwningUserNumber",
+                table: "Journal",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journal_EditingUserNumber",
+                table: "Journal",
+                column: "EditingUserNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journal_OwningUserNumber",
+                table: "Journal",
+                column: "OwningUserNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Journal_User_EditingUserNumber",
+                table: "Journal",
+                column: "EditingUserNumber",
+                principalTable: "User",
+                principalColumn: "UserNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Journal_User_OwningUserNumber",
+                table: "Journal",
+                column: "OwningUserNumber",
+                principalTable: "User",
+                principalColumn: "UserNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Journal_User_EditingUserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Journal_User_OwningUserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Journal_EditingUserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Journal_OwningUserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropColumn(
+                name: "EditingUserNumber",
+                table: "Journal");
+
+            migrationBuilder.DropColumn(
+                name: "OwningUserNumber",
+                table: "Journal");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserNumber",
+                table: "Journal",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journal_UserNumber",
+                table: "Journal",
+                column: "UserNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Journal_User_UserNumber",
+                table: "Journal",
+                column: "UserNumber",
+                principalTable: "User",
+                principalColumn: "UserNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/Journal.cs
+++ Models/Journal.cs
@@ -12,9 +12,15 @@ namespace Class4_Journals.Models
         [StringLength(500, MinimumLength = 3, ErrorMessage = "The length of Name should be more than 3 and less then 500 characters.")]
         public string JournalCotent { get; set; }
 
-        public int UserNumber { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Created { get; set; }
 
-        public User User { get; set; }
+        public int? OwningUserNumber { get; set; }
+        public int? EditingUserNumber { get; set; }
+
+        public User? OwningUser { get; set; }
+        public User? EditingUser { get; set; }
 
         [ForeignKey("JournalNumber")]
         public ICollection<Comment> Comments { get; set; }
